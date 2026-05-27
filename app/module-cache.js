19f2269bba5cd61d2aacbeee3334b1de455a50b6
Danny <danny.v.der.pol@gmail.com>
2026-05-27 10:03:00 +0200

    add debug log

--- app/module-cache.js
+++ app/module-cache.js
@@ -1,12 +1,15 @@
 /* Cache of the modules, prevent loading multiple times */
 
+import { debugLog } from "./logger.js";
+
 export const moduleCache = new Map();
 
 export async function loadModule(module_id)
 {
   if (!moduleCache.has(module_id))
   {
-    var path = "../calculators/" + module_id + ".js";
+    const path = "../calculators/" + module_id + ".js";
+    debugLog("Loading module from:", path)
     moduleCache.set(module_id, import(path));
   }
   return moduleCache.get(module_id);
